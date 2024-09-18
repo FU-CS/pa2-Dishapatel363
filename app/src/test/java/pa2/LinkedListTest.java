381e07bc28b778ee9d99fd6b1e2a4729ee8a54c2
Disha Patel <dishapatel@Dishas-MacBook-Pro-2.local>
2024-09-17 23:41:03 -0400

    programming assignment 2, forth try

--- app/src/test/java/pa2/LinkedListTest.java
+++ app/src/test/java/pa2/LinkedListTest.java
@@ -24,4 +24,39 @@ public class LinkedListTest {
         assertEquals("1 3 ", list.traverse());
     }
 
+    @Test
+    void testTraverse(){
+        LinkedList list = new LinkedList(); 
+        assertEquals("", list.reverse());
+        list.add(1);
+        assertEquals("1 ", list.reverse());
+        list.add(3);
+        list.add(2);
+        assertEquals("3 2 1 ", list.reverse());
+    }
+
+    @Test
+    void testReverse() {
+        LinkedList list = new LinkedList();
+
+        assertEquals("", list.reverse());
+        list.add(1);
+        list.add(3);
+        list.add(2);
+        assertEquals("1 2 3 ", list.traverse());
+    }
+
+    @Test
+    void testMerge() {
+        LinkedList list1 = new LinkedList();
+        list1.add(1);
+        list1.add(3);
+        list1.add(5);
+        LinkedList list2 = new LinkedList();
+        list2.add(2);
+        list2.add(4);
+        list2.add(6);
+        LinkedList mergedList = LinkedList.merge(list1, list2);
+        assertEquals("1 2 3 4 5 6 ", mergedList.traverse());
+    }
 }
